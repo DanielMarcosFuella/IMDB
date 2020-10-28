22985b9f5356b2d479eac3dfddb1b0a8e8bc234a
Xavi <javierhyws@gmail.com>
2020-10-28 17:23:51 +0100

    Actulizado v1

--- imdb.js
+++ imdb.js
@@ -1,42 +1,42 @@
 "use strict";
 exports.__esModule = true;
-var Profesional_1 = require("./Profesional");
-var Movie_1 = require("./Movie");
+exports.IMDB = void 0;
 var IMDB = /** @class */ (function () {
     function IMDB(movies) {
         this.movies = movies;
     }
     return IMDB;
 }());
-var profesional1 = new Profesional_1.Professional("Manolo", 33, "Masculino", 89, 168, "marron", "azul", "humana", false, "española", 2, "Actor");
-var profesional2 = new Profesional_1.Professional("Benito", 34, "Masculino", 95, 166, "marron", "marron", "humana", false, "española", 1, "Actor");
-var profesional3 = new Profesional_1.Professional("Maria", 31, "Femenino", 82, 176, "rubio", "azuñ", "humana", false, "española", 0, "Actor");
-var profesional4 = new Profesional_1.Professional("Javier", 39, "Masculino", 92, 176, "marron", "azul", "humana", false, "española", 0, "Actor");
-var profesional5 = new Profesional_1.Professional("Esther", 28, "Femenino", 65, 162, "marron", "verde", "humana", false, "española", 0, "Actor");
-var profesional6 = new Profesional_1.Professional("Monica", 38, "Femenino", 84, 176, "marron", "azul", "humana", false, "española", 1, "Actor");
-var profesional7 = new Profesional_1.Professional("Daniela", 43, "Femenino", 82, 172, "rubio", "marron", "humana", false, "española", 0, "Actor");
-var profesional8 = new Profesional_1.Professional("Juanjo", 45, "Masculino", 88, 176, "rubio", "verde", "humana", false, "española", 0, "Actor");
-var productor = new Profesional_1.Professional("Manolo", 33, "Masculino", 82, 175, "marron", "marron", "humana", false, "española", 1, "Productor");
-var director = new Profesional_1.Professional("Michael", 51, "Masculino", 100, 165, "marron", "azul", "humana", true, "inglesa", 2, "Escritor");
-var escritor = new Profesional_1.Professional("Jhon", 45, "Masculino", 120, 175, "rubio", "azul", "humana", false, "colombiana", 0, "Director");
-var p1 = new Movie_1.Movie("Manolo y Benito The Movie", 2021, "Española", "Comedia");
-var p2 = new Movie_1.Movie("Manolo y Benito Contratacan ", 2022, "Española", "Comedia");
-p1.actors = [profesional1, profesional2];
-p1.director = director;
-p1.producer = productor;
-p1.writer = escritor;
-p1.isMCU = true;
-p1.language = "Española";
-p1.distributor = "Movie Records";
-p1.plataform = "Disney+";
-p2.actors = [profesional1, profesional2, profesional5];
-p2.director = director;
-p2.producer = productor;
-p2.writer = escritor;
-p2.isMCU = true;
-p2.language = "Española";
-p2.distributor = "Movie Records";
-p2.plataform = "Netflix";
-var arrMovies = [p1, p2];
-var imdb = new IMDB(arrMovies);
-console.log(imdb.movies.toString());
+exports.IMDB = IMDB;
+// let profesional1 = new Professional("Manolo", 33, "Masculino", 89,168,"marron", "azul", "humana", false, "española", 2, "Actor" )
+// let profesional2 = new Professional("Benito", 34, "Masculino", 95,166,"marron", "marron", "humana", false, "española", 1, "Actor" )
+// let profesional3 = new Professional("Maria", 31, "Femenino", 82,176,"rubio", "azuñ", "humana", false, "española", 0, "Actor" )
+// let profesional4 = new Professional("Javier", 39, "Masculino", 92,176,"marron", "azul", "humana", false, "española", 0, "Actor" )
+// let profesional5 = new Professional("Esther", 28, "Femenino", 65,162,"marron", "verde", "humana", false, "española", 0, "Actor" )
+// let profesional6 = new Professional("Monica", 38, "Femenino", 84,176,"marron", "azul", "humana", false, "española", 1, "Actor" )
+// let profesional7 = new Professional("Daniela", 43, "Femenino", 82,172,"rubio", "marron", "humana", false, "española", 0, "Actor" )
+// let profesional8 = new Professional("Juanjo", 45, "Masculino", 88,176,"rubio", "verde", "humana", false, "española", 0, "Actor" )
+// let productor = new Professional("Manolo", 33, "Masculino", 82,175,"marron", "marron", "humana", false, "española", 1, "Productor" )
+// let director = new Professional("Michael", 51, "Masculino", 100,165,"marron", "azul", "humana", true, "inglesa", 2, "Escritor" )
+// let escritor = new Professional("Jhon", 45, "Masculino", 120,175,"rubio", "azul", "humana", false, "colombiana", 0, "Director" )
+// let p1:Movie = new Movie("Manolo y Benito The Movie",2021,"Española","Comedia");
+// let p2:Movie = new Movie("Manolo y Benito Contratacan ",2022,"Española","Comedia");
+// p1.actors = [profesional1,profesional2];
+// p1.director = director;
+// p1.producer = productor;
+// p1.writer = escritor;
+// p1.isMCU = true;
+// p1.language = "Española";
+// p1.distributor = "Movie Records";
+// p1.plataform = "Disney+";
+// p2.actors = [profesional1,profesional2,profesional5];
+// p2.director = director;
+// p2.producer = productor;
+// p2.writer = escritor;
+// p2.isMCU = true;
+// p2.language = "Española";
+// p2.distributor = "Movie Records";
+// p2.plataform = "Netflix";
+// let arrMovies = [p1,p2]
+// let imdb:IMDB = new IMDB(arrMovies);
+// console.log(imdb.movies.toString())
--- imdb.ts
+++ imdb.ts
@@ -1,7 +1,7 @@
 import { Professional } from "./Profesional";
 import { Movie } from "./Movie";
 
-class IMDB {
+export class IMDB {
 
     public movies:Movie[];
     constructor(movies:Movie[]) {
@@ -9,43 +9,3 @@ class IMDB {
     }
 
 }
-
-let profesional1 = new Professional("Manolo", 33, "Masculino", 89,168,"marron", "azul", "humana", false, "española", 2, "Actor" )
-let profesional2 = new Professional("Benito", 34, "Masculino", 95,166,"marron", "marron", "humana", false, "española", 1, "Actor" )
-let profesional3 = new Professional("Maria", 31, "Femenino", 82,176,"rubio", "azuñ", "humana", false, "española", 0, "Actor" )
-let profesional4 = new Professional("Javier", 39, "Masculino", 92,176,"marron", "azul", "humana", false, "española", 0, "Actor" )
-let profesional5 = new Professional("Esther", 28, "Femenino", 65,162,"marron", "verde", "humana", false, "española", 0, "Actor" )
-let profesional6 = new Professional("Monica", 38, "Femenino", 84,176,"marron", "azul", "humana", false, "española", 1, "Actor" )
-let profesional7 = new Professional("Daniela", 43, "Femenino", 82,172,"rubio", "marron", "humana", false, "española", 0, "Actor" )
-let profesional8 = new Professional("Juanjo", 45, "Masculino", 88,176,"rubio", "verde", "humana", false, "española", 0, "Actor" )
-let productor = new Professional("Manolo", 33, "Masculino", 82,175,"marron", "marron", "humana", false, "española", 1, "Productor" )
-let director = new Professional("Michael", 51, "Masculino", 100,165,"marron", "azul", "humana", true, "inglesa", 2, "Escritor" )
-let escritor = new Professional("Jhon", 45, "Masculino", 120,175,"rubio", "azul", "humana", false, "colombiana", 0, "Director" )
-
-
-let p1:Movie = new Movie("Manolo y Benito The Movie",2021,"Española","Comedia");
-let p2:Movie = new Movie("Manolo y Benito Contratacan ",2022,"Española","Comedia");
-
-p1.actors = [profesional1,profesional2];
-p1.director = director;
-p1.producer = productor;
-p1.writer = escritor;
-p1.isMCU = true;
-p1.language = "Española";
-p1.distributor = "Movie Records";
-p1.plataform = "Disney+";
-
-p2.actors = [profesional1,profesional2,profesional5];
-p2.director = director;
-p2.producer = productor;
-p2.writer = escritor;
-p2.isMCU = true;
-p2.language = "Española";
-p2.distributor = "Movie Records";
-p2.plataform = "Netflix";
-
-let arrMovies = [p1,p2]
-
-let imdb:IMDB = new IMDB(arrMovies);
-
-console.log(imdb.movies.toString())
